33b11151bac1773767512d4edf31d56e8db50e6c
伊藤 武尊 <takeru.ito110632@e-seikatsu.co.jp>
2020-10-18 12:22:16 +0900

    候補が複数の時の探索を追加

--- src/solver.py
+++ src/solver.py
@@ -27,34 +27,56 @@ class Solver:
         return dict(zip(self.squares, chars))
 
     def solve(self):
-        for s,d in self.board.items():
-            if d in self.digits and not self._assign(s, d):
-                break
+        self.values = self._search(self._solve(self.values))
 
         utils.display(self.values, self.rows, self.cols)
 
-    def _assign(self, s, d):
-        values_ = self.values[s].replace(d, '')
-        return all(self._eliminate(s, d_) for d_ in values_)
+    def _solve(self, values):
+        for s, d in self.board.items():
+            if d in self.digits and not self._assign(values, s, d):
+                return False
+        return values
 
-    def _eliminate(self, s, d):
-        if d not in self.values[s]:
-            return True
-        self.values[s] = self.values[s].replace(d, '')
-
-        if len(self.values[s])==0:
+    def _search(self, values):
+        if not values:
             return False
-        elif len(self.values[s])==1:
-            d_ = self.values[s]
-            if not all(self._eliminate(s_, d_) for s_ in self.peers[s]):
+        if all(len(values[s])==1 for s in self.squares):
+            return values
+        tmp = dict((s, len(values[s])) for s in self.squares if len(values[s])>1)
+        s = min(tmp, key=tmp.get)
+        # n, s = ((len(values[s]), s) for s in self.squares if len(values[s])>1)
+        return self._some(self._search(self._assign(values.copy(), s, d)) for d in values[s])
+
+    def _assign(self, values, s, d):
+        values_ = values[s].replace(d, '')
+        if all(self._eliminate(values, s, d_) for d_ in values_):
+            return values
+        return False
+
+    def _eliminate(self, values, s, d):
+        if d not in values[s]:
+            return values
+        values[s] = values[s].replace(d, '')
+
+        if len(values[s])==0:
+            return False
+        elif len(values[s])==1:
+            d_ = values[s]
+            if not all(self._eliminate(values, s_, d_) for s_ in self.peers[s]):
                 return False
         
         for u in self.units[s]:
-            dplaces = [s for s in u if d in self.values[s]]
+            dplaces = [s for s in u if d in values[s]]
             if len(dplaces )==0:
                 return False
             elif len(dplaces)==1:
-                if not self._assign(dplaces[0], d):
+                if not self._assign(values, dplaces[0], d):
                     return False
 
-        return True
+        return values
+
+    def _some(self, seq):
+        for e in seq:
+            if e: 
+                return e
+        return False
